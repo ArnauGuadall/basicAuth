e22836204c25328515ce079a0555d35557e2cea0
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2017-07-10 14:44:10 +0200

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+function auth(req, res, next) {
+  if (req.session.currentUser) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+}
+
+
+
+/* GET home page. */
+router.get('/',  function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+router.get('/secret', auth, function (req, res, next) {
+  res.render('secret');
+});
+
+
+module.exports = router;
